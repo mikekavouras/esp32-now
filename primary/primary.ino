cab5f5e2f6b8f63bbaadd634697ed0fd0ec51c47
Mike Kavouras <mikekavouras@github.com>
2023-03-10 12:55:35 -0500

    Firsties

---
+++ primary/primary.ino
@@ -0,0 +1,74 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include "AiEsp32RotaryEncoder.h"
+#include "Arduino.h"
+
+#define ROTARY_ENCODER_A_PIN 32
+#define ROTARY_ENCODER_B_PIN 21
+#define ROTARY_ENCODER_BUTTON_PIN 22
+#define ROTARY_ENCODER_VCC_PIN -1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
+#define ROTARY_ENCODER_STEPS 2
+
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+
+uint8_t address[] = {0x8C, 0xAA, 0xB5, 0x84, 0xFB, 0xA0};
+
+typedef struct struct_message {
+  int value;
+} struct_message;
+
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
+int esp_ok = 1;
+int added_peer = 1;
+int val = 127;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  /* Serial.println("sent"); */
+}
+
+void IRAM_ATTR readEncoderISR()
+{
+	rotaryEncoder.readEncoder_ISR();
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  memcpy(peerInfo.peer_addr, address, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    added_peer = 0;
+    return;
+  }
+
+	rotaryEncoder.begin();
+	rotaryEncoder.setup(readEncoderISR);
+	rotaryEncoder.setBoundaries(0, 255 * ROTARY_ENCODER_STEPS, true);
+  rotaryEncoder.disableAcceleration();
+}
+
+void loop() {
+	if (rotaryEncoder.encoderChanged()) {
+    val = rotaryEncoder.readEncoder() / ROTARY_ENCODER_STEPS;
+    Serial.println(val);
+    sendData();
+	}
+}
+
+void sendData() {
+  myData.value = val;
+  esp_now_send(address, (uint8_t *) &myData, sizeof(myData));
+}
---
+++ secondary/secondary.ino
@@ -0,0 +1,52 @@
+#include <Adafruit_NeoPixel.h>
+#include <esp_now.h>
+#include <WiFi.h>
+
+typedef struct struct_message {
+    int value; } struct_message;
+
+struct_message myData;
+
+#define NUMPIXELS 30
+#define LED_PIN 32
+Adafruit_NeoPixel strip(NUMPIXELS, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.println(myData.value);
+  uint32_t c = wheel(myData.value);
+  for (int i = 0; i < NUMPIXELS; i++) {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  strip.begin();
+
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {}
+
+uint32_t wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3, 50);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3, 50);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 50);
+}
